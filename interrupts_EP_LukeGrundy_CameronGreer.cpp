482dc2da1d9cae0c57b7a45361fee9874a957800
Luke Grundy <LukeGrundy@cmail.carleton.ca>
2025-11-30 16:41:53 -0500

    Just formating

--- interrupts_EP_LukeGrundy_CameronGreer.cpp
+++ interrupts_EP_LukeGrundy_CameronGreer.cpp
@@ -1,60 +1,66 @@
 /**
  * @file interrupts.cpp
  * @author Sasisekhar Govind
- * 
+ *
  * @author Luke Grundy
  * @author Cameron Greer
- * 
+ *
  * @brief template main.cpp file for Assignment 3 Part 1 of SYSC4001
- * 
+ *
  * @date November 25th, 2025
- * 
+ *
  */
 
-#include<interrupts_LukeGrundy_CameronGreer.hpp>
+#include <interrupts_LukeGrundy_CameronGreer.hpp>
 
-void FCFS(std::vector<PCB> &ready_queue) {
-    std::sort( 
-                ready_queue.begin(),
-                ready_queue.end(),
-                []( const PCB &first, const PCB &second ){
-                    return (first.arrival_time > second.arrival_time); 
-                } 
-            );
+void FCFS(std::vector<PCB> &ready_queue)
+{
+    std::sort(
+        ready_queue.begin(),
+        ready_queue.end(),
+        [](const PCB &first, const PCB &second)
+        {
+            return (first.arrival_time > second.arrival_time);
+        });
 }
 
 // places the process to be executed next at the front of the ready queue (based on PID)
-void EP(std::vector<PCB> &ready_queue) {
-    if (ready_queue.size() <= 1) {
+void EP(std::vector<PCB> &ready_queue)
+{
+    if (ready_queue.size() <= 1)
+    {
         return;
     }
     int best = 0;
-    for (int i = 1; i < ready_queue.size(); i++) {
-        if (ready_queue[i].PID < ready_queue[best].PID){
+    for (int i = 1; i < ready_queue.size(); i++)
+    {
+        if (ready_queue[i].PID < ready_queue[best].PID)
+        {
             best = i;
         }
     }
     std::swap(ready_queue[best], ready_queue.back());
 }
 
-std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std::vector<PCB> list_processes) {
+std::tuple<std::string /* add std::string for bonus mark */> run_simulation(std::vector<PCB> list_processes)
+{
 
-    std::vector<PCB> ready_queue;   //The ready queue of processes
-    std::vector<PCB> wait_queue;    //The wait queue of processes
-    std::vector<PCB> job_list;      //A list to keep track of all the processes. This is similar
-                                    //to the "Process, Arrival time, Burst time" table that you
-                                    //see in questions. You don't need to use it, I put it here
-                                    //to make the code easier :).
+    std::vector<PCB> ready_queue; // The ready queue of processes
+    std::vector<PCB> wait_queue;  // The wait queue of processes
+    std::vector<PCB> job_list;    // A list to keep track of all the processes. This is similar
+                               // to the "Process, Arrival time, Burst time" table that you
+                               // see in questions. You don't need to use it, I put it here
+                               // to make the code easier :).
 
     unsigned int current_time = 0;
     PCB running;
 
-    //Initialize an empty running process
+    // Initialize an empty running process
     idle_CPU(running);
 
     std::string execution_status;
 
-    //make the output table (the header row)
+    // make the output table (the header row)
     execution_status = print_exec_header();
 
     //Loop while till there are no ready or waiting processes.
@@ -66,16 +72,18 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
         // 2) Manage the wait queue
         // 3) Schedule processes from the ready queue
 
-        //Population of ready queue is given to you as an example.
-        //Go through the list of proceeses
-        for(auto &process : list_processes) {
-            if(process.arrival_time == current_time) {//check if the AT = current time
-                //if so, assign memory and put the process into the ready queue
+        // Population of ready queue is given to you as an example.
+        // Go through the list of proceeses
+        for (auto &process : list_processes)
+        {
+            if (process.arrival_time == current_time)
+            { // check if the AT = current time
+                // if so, assign memory and put the process into the ready queue
                 assign_memory(process);
 
-                process.state = READY;  //Set the process state to READY
-                ready_queue.push_back(process); //Add the process to the ready queue
-                job_list.push_back(process); //Add it to the list of processes
+                process.state = READY;          // Set the process state to READY
+                ready_queue.push_back(process); // Add the process to the ready queue
+                job_list.push_back(process);    // Add it to the list of processes
 
                 execution_status += print_exec_status(current_time, process.PID, NEW, READY);
             }
@@ -89,11 +97,11 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
 		ready_queue.push_back(wait_queue[i]);
 	        sync_queue(job_list, wait_queue[i]);
 
-		execution_status += print_exec_status(current_time, wait_queue[i].PID, WAITING, READY);
+                execution_status += print_exec_status(current_time, wait_queue[i].PID, WAITING, READY);
 
-		wait_queue.erase(wait_queue.begin() + i);
-	    }
-	}
+                wait_queue.erase(wait_queue.begin() + i);
+            }
+        }
         /////////////////////////////////////////////////////////////////
 
         //////////////////////////SCHEDULER//////////////////////////////
@@ -127,45 +135,48 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
 	/////////////////////////////////////////////////////////////////
     }
 
-    //Close the output table
+    // Close the output table
     execution_status += print_exec_footer();
 
     return std::make_tuple(execution_status);
 }
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char** argv) {
-
-    //Get the input file from the user
-    if(argc != 2) {
+    // Get the input file from the user
+    if (argc != 2)
+    {
         std::cout << "ERROR!\nExpected 1 argument, received " << argc - 1 << std::endl;
         std::cout << "To run the program, do: ./interrutps <your_input_file.txt>" << std::endl;
         return -1;
     }
 
-    //Open the input file
+    // Open the input file
     auto file_name = argv[1];
     std::ifstream input_file;
     input_file.open(file_name);
 
-    //Ensure that the file actually opens
-    if (!input_file.is_open()) {
+    // Ensure that the file actually opens
+    if (!input_file.is_open())
+    {
         std::cerr << "Error: Unable to open file: " << file_name << std::endl;
         return -1;
     }
 
-    //Parse the entire input file and populate a vector of PCBs.
-    //To do so, the add_process() helper function is used (see include file).
+    // Parse the entire input file and populate a vector of PCBs.
+    // To do so, the add_process() helper function is used (see include file).
     std::string line;
     std::vector<PCB> list_process;
-    while(std::getline(input_file, line)) {
+    while (std::getline(input_file, line))
+    {
         auto input_tokens = split_delim(line, ", ");
         auto new_process = add_process(input_tokens);
         list_process.push_back(new_process);
     }
     input_file.close();
 
-    //With the list of processes, run the simulation
+    // With the list of processes, run the simulation
     auto [exec] = run_simulation(list_process);
 
     write_output(exec, "execution.txt");
